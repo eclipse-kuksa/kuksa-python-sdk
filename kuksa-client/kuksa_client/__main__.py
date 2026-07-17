d6b4b1c66186bd51542512b303de7af609493a3e
Sebastian Schildt <sebastian.schildt@hs-heilbronn.de>
2026-07-17 09:42:36 +0200

    Fix v1/v2 switching logic for subscribe in CLI client and fix subscription rendering due to changed cmd2 API
    Signed-off-by: Sebastian Schildt <sebastian.schildt@hs-heilbronn.de>

--- kuksa-client/kuksa_client/__main__.py
+++ kuksa-client/kuksa_client/__main__.py
@@ -129,14 +129,13 @@ class TestClient(Cmd):
 
     def subscribeCallback(self, logPath, resp):
         if logPath is None:
-            with self.terminal_lock:
-                self.async_alert(
-                    highlight(
-                        json.dumps(json.loads(resp), indent=2),
-                        lexers.JsonLexer(),
-                        formatters.TerminalFormatter(),
-                    )
+            self.add_alert(
+                msg=highlight(
+                    json.dumps(json.loads(resp), indent=2),
+                    lexers.JsonLexer(),
+                    formatters.TerminalFormatter(),
                 )
+            )
         else:
             with logPath.open("a", encoding="utf-8") as logFile:
                 logFile.write(resp + "\n")
